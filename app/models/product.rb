ea21bf9a87c187faaeb2d7307f623aabe019740b
Randall Mardus <randalljmardus@gmail.com>
2014-10-28 12:39:55 -0400

    store display commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,3 +7,7 @@ class Product < ActiveRecord::Base
 		with: %r{\.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.'
 	}
 end
+
+def self.latest
+	Product.order(:updated_at).last
+end
